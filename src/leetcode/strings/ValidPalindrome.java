a0527633333c771e17eb30f7a3aa8e30a4318880
Bryant Ferguson <btbf569708@gmail.com>
2026-03-15 02:14:25 -0400

    Added another solution to isPalindrome

--- src/leetcode/strings/ValidPalindrome.java
+++ src/leetcode/strings/ValidPalindrome.java
@@ -22,6 +22,8 @@ Since an empty string reads the same forward and backward, it is a palindrome.
  */
 public class ValidPalindrome {
     public static boolean isPalindrome(String s) {
+        //StringBuilder solution
+        /*
         //creates the new messsage with only numbers or digits
         if(s.equals("")){
             return true;
@@ -39,5 +41,39 @@ public class ValidPalindrome {
         String forward = text.toString();
         String reverse = text.reverse().toString();
         return reverse.equals(forward);
+         */
+
+        //normal string solution
+        if (s.equals("")) {
+            return true;
+        }
+        //gets rid of whitespace
+        s = s.toLowerCase().replace(" ", "");
+
+        //this section traverses through string s backwards and builds a new reversed string
+        String reverse = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            //only concats the char to reverse if it's a letter or digit
+            if (Character.isLetterOrDigit(c)) {
+                reverse += "" + c;
+            }
+        }
+
+        //this section reverses the reversed string
+        String forward = "";
+        for (int i = reverse.length() - 1; i >= 0; i--) {
+            char c = reverse.charAt(i);
+            if (Character.isLetterOrDigit(c)) {
+                forward += "" + c;
+            }
+        }
+
+       // System.out.println("Reverse: " + reverse);
+        //System.out.println("Forward: " + forward);
+
+        //returns if the reverse and forawrd strings are equal to each other
+        return reverse.equals(forward);
     }
+
 }
